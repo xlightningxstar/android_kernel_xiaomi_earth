241f4c42737ea812e0c8975963cac6efff466fbd
Yonghong Song <yhs@fb.com>
2018-11-20 14:08:20 -0800

    bpf: fix a compilation error when CONFIG_BPF_SYSCALL is not defined
    Kernel test robot (lkp@intel.com) reports a compilation error at
      https://www.spinics.net/lists/netdev/msg534913.html
    introduced by commit 838e96904ff3 ("bpf: Introduce bpf_func_info").
    
    If CONFIG_BPF is defined and CONFIG_BPF_SYSCALL is not defined,
    the following error will appear:
      kernel/bpf/core.c:414: undefined reference to `btf_type_by_id'
      kernel/bpf/core.c:415: undefined reference to `btf_name_by_offset'
    
    When CONFIG_BPF_SYSCALL is not defined,
    let us define stub inline functions for btf_type_by_id()
    and btf_name_by_offset() in include/linux/btf.h.
    This way, the compilation failure can be avoided.
    
    Fixes: 838e96904ff3 ("bpf: Introduce bpf_func_info")
    Reported-by: kbuild test robot <lkp@intel.com>
    Cc: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- include/linux/btf.h
+++ include/linux/btf.h
@@ -50,6 +50,7 @@ u32 btf_id(const struct btf *btf);
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 bool btf_member_is_reg_int(const struct btf *btf, const struct btf_type *s,
 			   const struct btf_member *m,
@@ -64,6 +65,8 @@ bool btf_member_is_reg_int(const struct btf *btf, const struct btf_type *s,
 =======
 bool btf_type_is_reg_int(const struct btf_type *t, u32 expected_size);
 >>>>>>> dadbcff5dd52 (bpf: add bpffs pretty print for cgroup local storage maps)
+=======
+>>>>>>> 0d66cc14e55b (bpf: fix a compilation error when CONFIG_BPF_SYSCALL is not defined)
 
 #ifdef CONFIG_BPF_SYSCALL
 const struct btf_type *btf_type_by_id(const struct btf *btf, u32 type_id);
@@ -81,6 +84,9 @@ static inline const char *btf_name_by_offset(const struct btf *btf,
 	return NULL;
 }
 #endif
+<<<<<<< HEAD
 >>>>>>> d1efffd3c35e (bpf: introduce bpf_spin_lock)
+=======
+>>>>>>> 0d66cc14e55b (bpf: fix a compilation error when CONFIG_BPF_SYSCALL is not defined)
 
 #endif
